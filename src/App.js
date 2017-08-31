8ac2561d445c0e34d0322c552a9877e7b5ae282f
Anders Wood <anderswood@gmail.com>
2017-08-31 16:36:53 -0600

    flesh out inputs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import moment from 'moment';
 
-import {key} from './key';
+// import { key } from './key';
 import Inputs from './Inputs';
 import Forecast from './Forecast';
+import { stubbedData } from './stubbed';
 
 class App extends Component {
 
@@ -17,7 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <Inputs />
-        <section className='current-container'>Howdy! It's ${} and your local weather is ${}</section>
+        <section className='current-container'>Howdy! It's {moment.unix(stubbedData.currently.time).format('h:mm a')} on {moment.unix(stubbedData.currently.time).format('MMMM Do')} and your local weather is {stubbedData.currently.summary}</section>
         <Forecast />
       </div>
     );
